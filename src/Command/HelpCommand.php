3087ff7ff6d5ef181566bbe19889e926bae46076
Ben Ramsey <ben@shootproof.com>
2015-01-28 11:03:53 -0600

    Docblock comments for Command\HelpCommand

--- src/Command/HelpCommand.php
+++ src/Command/HelpCommand.php
@@ -14,10 +14,19 @@ namespace ShootProof\Cli\Command;
 use Aura\Cli\Help;
 use Aura\Cli\Stdio;
 
+/**
+ * Provides the shootproof-cli help command
+ */
 class HelpCommand implements HelpableCommandInterface
 {
+    /**
+     * @var string
+     */
     public static $usage = 'help [command]';
 
+    /**
+     * @var string
+     */
     public static $description = <<<TEXT
 Displays this help screen, or help for a specific command.
 
@@ -29,8 +38,18 @@ Displays this help screen, or help for a specific command.
         accesslevel
 TEXT;
 
+    /**
+     * Command line standard input/output
+     * @var Stdio
+     */
     protected $stdio;
 
+    /**
+     * Sets up the help message output for the help command
+     *
+     * @param Help $help The CLI help object used to configure help messages
+     * @return Help
+     */
     public static function configureHelp(Help $help)
     {
         $help->setUsage(self::$usage);
@@ -40,11 +59,22 @@ TEXT;
         return $help;
     }
 
+    /**
+     * Constructs the help command
+     *
+     * @param Stdio $stdio Command line standard input/output
+     */
     public function __construct(Stdio $stdio)
     {
         $this->stdio = $stdio;
     }
 
+    /**
+     * Called when this object is called as a function
+     *
+     * @param Help $help
+     * @param HelpableCommandInterface $subCommand
+     */
     public function __invoke(Help $help, HelpableCommandInterface $subCommand = null)
     {
         if ($subCommand !== null) {
