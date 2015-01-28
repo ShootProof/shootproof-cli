061414f25e6a257af13409c70005b5bc431cb0b5
Ben Ramsey <ben@shootproof.com>
2015-01-28 09:19:38 -0600

    Add file heading comment to all scripts

--- src/Validators/ValidatorException.php
+++ src/Validators/ValidatorException.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * This file is part of the ShootProof command line tool.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright Copyright (c) ShootProof, LLC (https://www.shootproof.com)
+ * @license http://opensource.org/licenses/MIT MIT
+ */
 
 namespace ShootProof\Cli\Validators;
 
