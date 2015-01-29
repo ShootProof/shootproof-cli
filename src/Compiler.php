d8862e490363696c65d743344a11e48d08c1e5fe
Ben Ramsey <ben@shootproof.com>
2015-01-29 14:46:51 -0600

    Adding copyright and license notice to the phar stub

--- src/Compiler.php
+++ src/Compiler.php
@@ -133,6 +133,16 @@ class Compiler
         $stub = <<<EOF
 #!/usr/bin/env php
 <?php
+/**
+ * This file is part of the ShootProof command line tool.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright Copyright (c) ShootProof, LLC (https://www.shootproof.com)
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+
 Phar::mapPhar('shootproof-cli.phar');
 require 'phar://shootproof-cli.phar/shootproof-cli/bin/shootproof-cli';
 __HALT_COMPILER();
